a43d6c8eedb427155b653174780a2a66e6ed99a6
Senad Hajric <senadhajric3@gmail.com>
2024-10-08 10:06:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bildergalerie_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bildergalerie_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bildergalerie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
